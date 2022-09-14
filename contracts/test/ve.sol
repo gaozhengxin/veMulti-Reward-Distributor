e39ebd0ab66f6c7e303f651a395593590781347f
gaozhengxin <zhengxingao@live.cn>
2022-09-14 10:40:07 +0800

    remove checkpoint trigger

--- contracts/test/ve.sol
+++ contracts/test/ve.sol
@@ -3,16 +3,9 @@ pragma solidity ^0.8.0;
 
 /**
  * @dev Pseudo ve contract for testing purposes
- * auto checkpoint
  * reward distributor
  */
 contract ve {
-    event Checkpoint(uint256 timestamp);
-
-    function checkpoint() external {
-        emit Checkpoint(block.timestamp);
-    }
-
     uint256 _totalSupply = 1000000000000000000000000;
 
     function totalSupplyAtT(uint256 t) external view returns (uint256) {
